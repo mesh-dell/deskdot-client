a1fcfad3eee1897d921789205a08cb8adda46bf7
mesh-dell <mwaimonicas@gmail.com>
2024-09-12 23:37:36 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className=" p-32 text-3xl font-bold underline text-yellow-300 bg-slate-600">
+        Hello World
+      </h1>
+    </>
+  );
+}
+
+export default App;
