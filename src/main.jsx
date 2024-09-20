af8f08afa87d998a0b9461f9ae294b7d1fc99c72
mesh-dell <mwaimonicas@gmail.com>
2024-09-20 16:54:17 +0300

    feat: Set up react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import App from "./App.jsx";
 import "./index.css";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Gear Five</div>,
+  },
+]);
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
