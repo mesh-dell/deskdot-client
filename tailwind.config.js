097da5a061c0960fa5d0cdada93ea8dc3cded2b9
mesh-dell <mwaimonicas@gmail.com>
2024-09-28 17:39:15 +0300

    feat(colors): Add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      white: "#ffffff",
+      "dark-green": "#1A2E35",
+      "light-grey": "#808782",
+      "light-green": "#5AE4A7",
+      "medium-blue": "#62929E",
+      "light-white": "#F8FAF9",
+    },
     extend: {},
   },
   plugins: [],
