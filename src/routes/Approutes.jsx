a5389565613fea44ee7052c94757471c403cbfec
mesh-dell <mwaimonicas@gmail.com>
2024-09-29 19:20:33 +0300

    feat(Error): Add an error page to display errors

--- src/routes/Approutes.jsx
+++ src/routes/Approutes.jsx
@@ -1,10 +1,12 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "../pages/error-page";
 
 export default function AppRoutes() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <div>Home Page</div>,
+      errorElement: <ErrorPage />,
     },
   ]);
 
