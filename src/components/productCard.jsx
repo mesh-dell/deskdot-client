bfafb27817927e630de5e47e678370e07a036d9b
mesh-dell <mwaimonicas@gmail.com>
2024-09-30 22:35:15 +0300

    feat(product): Add responsivenss to product section

--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function ProductCard() {
   return (
-    <div className="text-dark-green bg-light-white space-y-2 p-3 text-center">
+    <div className="text-dark-green bg-light-white space-y-2 p-3 text-center w-full flex flex-col items-center md:w-fit">
       <div className="bg-light-grey h-48 w-52">Image</div>
       <h1 className="text-xl">Visocin Stapeler</h1>
       <h1 className="font-semibold">Ksh. 450</h1>
--- src/components/productSection.jsx
+++ src/components/productSection.jsx
@@ -4,7 +4,7 @@ export default function ProductSection() {
   return (
     <div>
       <h1 className="text-center text-xl text-dark-green mb-5">Here are some of our products</h1>
-      <div className="flex flex-col items-center md:flex-row md:justify-evenly space-y-5">
+      <div className="flex flex-col items-center md:flex-row md:justify-evenly space-y-5 md:space-y-0">
         <ProductCard />
         <ProductCard />
         <ProductCard />
