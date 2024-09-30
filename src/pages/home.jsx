0c716ceb7d77e0bbadd1030249f676cedf3daa7b
mesh-dell <mwaimonicas@gmail.com>
2024-09-30 22:15:28 +0300

    feat(Home): Add produxt section

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,11 @@
 import NavBar from "../components/navbar";
 import HeroesSection from "../components/heroes";
+import ProductSection from "../components/productSection";
 
 export default function HomePage() {
   return (
-    <div className="mx-6 md:mx-10 *:mb-10">
-      <NavBar /> <HeroesSection />
+    <div className="mx-6 *:mb-10 md:mx-10">
+      <NavBar /> <HeroesSection /> <ProductSection />
     </div>
   );
 }
