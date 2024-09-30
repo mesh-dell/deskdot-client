bbf4cd3a265756e80816788a2f4e380c5e6229b4
mesh-dell <mwaimonicas@gmail.com>
2024-09-30 20:59:54 +0300

    feat(heroes): Create heroes section component

---
+++ src/components/heroes.jsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+
+export default function HeroesSection() {
+  return (
+    <div className="text-dark-green flex-col text-center md:flex md:flex-row md:items-center md:justify-evenly md:text-left">
+      <div>
+        <h1 className="mb-3 text-2xl font-bold md:text-5xl">Deskdot</h1>
+        <h3 className="text-light-grey mb-6 md:text-xl">
+          Your One-Stop Shop for Smart Office Solutions
+        </h3>
+        <Link
+          className="bg-dark-green text-white rounded-md px-4 py-2 md:px-4 md:py-2 md:text-xl"
+          to={"/signup"}
+        >
+          Get Started
+        </Link>
+      </div>
+      <img
+        className="md:my-0 md:max-w-lg my-3"
+        src="/illustrationHero.png"
+        alt="Heroes section image"
+      />
+    </div>
+  );
+}
