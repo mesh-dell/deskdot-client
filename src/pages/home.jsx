a100595c03a6d961d59c96cd173bc4c945e27eae
mesh-dell <mwaimonicas@gmail.com>
2024-09-30 22:20:42 +0300

    feat(Home): Add call to action section

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,12 @@
 import NavBar from "../components/navbar";
 import HeroesSection from "../components/heroes";
 import ProductSection from "../components/productSection";
+import CallToAction from "../components/cta";
 
 export default function HomePage() {
   return (
     <div className="mx-6 *:mb-10 md:mx-10">
-      <NavBar /> <HeroesSection /> <ProductSection />
+      <NavBar /> <HeroesSection /> <ProductSection /> <CallToAction />
     </div>
   );
 }
