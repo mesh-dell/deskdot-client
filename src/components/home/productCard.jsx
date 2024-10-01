276c8c55229fe371f572c23976bb925706856289
mesh-dell <mwaimonicas@gmail.com>
2024-10-01 20:07:10 +0300

    refactor(Product): Wrap product card in link component

--- src/components/home/productCard.jsx
+++ src/components/home/productCard.jsx
@@ -1,14 +1,16 @@
 import { Link } from "react-router-dom";
 
-export default function ProductCard() {
+export default function ProductCard({ productId }) {
   return (
-    <div className="text-dark-green bg-light-white space-y-2 p-3 text-center w-full flex flex-col items-center md:w-fit">
-      <div className="bg-light-grey h-48 w-52">Image</div>
-      <h1 className="text-xl">Visocin Stapeler</h1>
-      <h1 className="font-semibold">Ksh. 450</h1>
-      <button className="bg-dark-green text-white rounded-md px-4 py-2">
-        Add to cart
-      </button>
-    </div>
+    <Link to={"/shop/:productId"}>
+      <div className="flex w-full flex-col items-center space-y-2 bg-light-white p-3 text-center text-dark-green md:w-fit">
+        <div className="h-48 w-52 bg-light-grey">Image</div>
+        <h1 className="text-xl">Visocin Stapeler</h1>
+        <h1 className="font-semibold">Ksh. 450</h1>
+        <button className="rounded-md bg-dark-green px-4 py-2 text-white">
+          Add to cart
+        </button>
+      </div>
+    </Link>
   );
 }
