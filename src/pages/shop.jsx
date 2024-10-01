47054424f9b651e6a85f5f4a81ba053e6c91870f
mesh-dell <mwaimonicas@gmail.com>
2024-10-01 19:57:01 +0300

    feat(Shop): Create shop page

---
+++ src/pages/shop.jsx
@@ -0,0 +1,10 @@
+import NavBar from "../components/home/navbar";
+import BreadCrumb from "../components/shop/breadcrumb";
+
+export default function Shop() {
+  return (
+    <div className="mx-6 *:mb-10 md:mx-10">
+      <NavBar /> <BreadCrumb />
+    </div>
+  );
+}
