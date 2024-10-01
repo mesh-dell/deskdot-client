ce33d827b2f346c877ae27be99de4e524cdc91a0
mesh-dell <mwaimonicas@gmail.com>
2024-10-01 11:30:02 +0300

    feat(Home): Add footer to home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,11 +2,13 @@ import NavBar from "../components/navbar";
 import HeroesSection from "../components/heroes";
 import ProductSection from "../components/productSection";
 import CallToAction from "../components/cta";
+import Footer from "../components/footer";
 
 export default function HomePage() {
   return (
     <div className="mx-6 *:mb-10 md:mx-10">
       <NavBar /> <HeroesSection /> <ProductSection /> <CallToAction />
+      <Footer />
     </div>
   );
 }
