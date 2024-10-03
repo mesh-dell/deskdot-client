1acdc6efb13e5d6f6bb8f4ebcffe7acb22a219c6
mesh-dell <mwaimonicas@gmail.com>
2024-10-03 23:28:51 +0300

    feat(account): Add personalInfo and recent orders components

--- src/pages/account.jsx
+++ src/pages/account.jsx
@@ -2,16 +2,28 @@ import SideBar from "../components/account/sidebar";
 import NavBar from "../components/home/navbar";
 import ProfileMenu from "../components/account/profileMenu";
 import LoginInfo from "../components/account/loginInfo";
+import PersonalInfo from "../components/account/personalInfo";
+import RecentOrders from "../components/account/recentOrders";
 
 export default function Account() {
   return (
     <div className="mx-6 *:mb-10 md:mx-10">
       <NavBar />
-      <div className="flex items-center justify-around md:block">
-        <LoginInfo />
-        <ProfileMenu />
+
+      <div className="md:flex items-start">
+        <div>
+          <div className="flex items-center justify-around md:block my-10 md:my-0">
+            <LoginInfo />
+            <ProfileMenu />
+          </div>
+          <SideBar />
+        </div>
+
+        <div className="md:ml-20 w-full">
+          <PersonalInfo />
+          <RecentOrders />
+        </div>
       </div>
-      <SideBar />
     </div>
   );
 }
