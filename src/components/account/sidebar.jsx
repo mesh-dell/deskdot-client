1becac0e637f78ebe6a99b47ebe88ce601a6b500
mesh-dell <mwaimonicas@gmail.com>
2024-10-03 22:02:53 +0300

    feat(account): Create sidebar for larger devices

---
+++ src/components/account/sidebar.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+export default function SideBar() {
+  return (
+    <div className="hidden w-1/6 space-y-20 text-dark-green md:block">
+      <div className="flex flex-col space-y-12 font-semibold">
+        <Link to={"/account"}>My Account</Link>
+        <Link to={"/account/orders"}>Orders</Link>
+        <Link to={"/account/settings"}>Settings</Link>
+      </div>
+
+      <button className="rounded-md bg-light-green px-6 py-1 font-semibold">
+        Log Out
+      </button>
+    </div>
+  );
+}
