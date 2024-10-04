4a84725553cebfaabc8f85addb0b33c49824b445
mesh-dell <mwaimonicas@gmail.com>
2024-10-04 14:20:25 +0300

    feat(accountInfo): Create accountInfo child component

---
+++ src/routes/account/accountInfo.jsx
@@ -0,0 +1,11 @@
+import PersonalInfo from "../../components/account/personalInfo";
+import RecentOrders from "../../components/account/recentOrders";
+
+export default function AccountInfo() {
+  return (
+    <div>
+      <PersonalInfo />
+      <RecentOrders />
+    </div>
+  );
+}
