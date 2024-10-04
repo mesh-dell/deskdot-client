08c734f2155d974fe92959e8cb5722efabf7a481
mesh-dell <mwaimonicas@gmail.com>
2024-10-04 16:54:08 +0300

    refactor(Links): Change Link to NavLink to identify active link

--- src/components/account/sidebar.jsx
+++ src/components/account/sidebar.jsx
@@ -1,12 +1,36 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 export default function SideBar() {
+  const location = useLocation().pathname;
+  const pattern = /account/i;
   return (
     <div className="hidden space-y-20 text-dark-green md:block">
       <div className="flex flex-col space-y-12 font-semibold">
-        <Link to={"/account"}>My Account</Link>
-        <Link to={"/account/orders"}>Orders</Link>
-        <Link to={"/account/settings"}>Settings</Link>
+        <NavLink
+          className={() =>
+            location == "/account" ? "border-b-2 border-dark-green" : ""
+          }
+          to={"/account"}
+        >
+          My Account
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? "border-b-2 border-dark-green" : ""
+          }
+          to={"/account/orders"}
+        >
+          Orders
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? "border-b-2 border-dark-green" : ""
+          }
+          to={"/account/settings"}
+        >
+          Settings
+        </NavLink>
       </div>
 
       <button className="rounded-md bg-light-green px-6 py-2 font-semibold">
