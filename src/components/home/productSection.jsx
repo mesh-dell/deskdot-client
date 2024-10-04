39802da57c42d67116c8e8393d5d522d2dfd64fa
mesh-dell <mwaimonicas@gmail.com>
2024-10-05 01:07:09 +0300

    refactor(Home): Modify apperance of product section on smaller screens

--- src/components/home/productSection.jsx
+++ src/components/home/productSection.jsx
@@ -3,11 +3,13 @@ import ProductCard from "./productCard";
 export default function ProductSection() {
   return (
     <div>
-      <h1 className="text-center text-xl text-dark-green mb-5">Here are some of our products</h1>
-      <div className="flex flex-col items-center md:flex-row md:justify-evenly space-y-5 md:space-y-0">
-        <ProductCard productId={1}/>
-        <ProductCard productId={2}/>
-        <ProductCard productId={3}/>
+      <h1 className="mb-5 text-center text-xl text-dark-green">
+        Here are some of our products
+      </h1>
+      <div className="flex flex-col justify-around space-y-5 md:flex-row md:space-y-0">
+        <ProductCard productId={1} />
+        <ProductCard productId={2} />
+        <ProductCard productId={3} />
       </div>
     </div>
   );
