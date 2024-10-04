21ef73b26f6ca5f9b46bf314a06af1044d7e3035
mesh-dell <mwaimonicas@gmail.com>
2024-10-04 14:19:21 +0300

    feat(account): Replace accountInfo component with outlet component

--- src/pages/account.jsx
+++ src/pages/account.jsx
@@ -2,26 +2,23 @@ import SideBar from "../components/account/sidebar";
 import NavBar from "../components/home/navbar";
 import ProfileMenu from "../components/account/profileMenu";
 import LoginInfo from "../components/account/loginInfo";
-import PersonalInfo from "../components/account/personalInfo";
-import RecentOrders from "../components/account/recentOrders";
+import { Outlet } from "react-router-dom";
 
 export default function Account() {
   return (
     <div className="mx-6 *:mb-10 md:mx-10">
       <NavBar />
-
-      <div className="md:flex items-start">
+      <div className="items-start md:flex">
         <div>
-          <div className="flex items-center justify-around md:block my-10 md:my-0">
+          <div className="my-10 flex items-center justify-around md:my-0 md:block">
             <LoginInfo />
             <ProfileMenu />
           </div>
           <SideBar />
         </div>
 
-        <div className="md:ml-20 w-full">
-          <PersonalInfo />
-          <RecentOrders />
+        <div className="w-full md:ml-20">
+          <Outlet />
         </div>
       </div>
     </div>
