74b670736306eb5c589870b73b7caef2165fdb8c
mesh-dell <mwaimonicas@gmail.com>
2024-10-06 23:43:49 +0300

    feat(routes): Add seller account route

--- src/routes/Approutes.jsx
+++ src/routes/Approutes.jsx
@@ -81,7 +81,7 @@ export default function AppRoutes() {
     },
     {
       path: "/account/seller",
-      element: <div>Seller Account</div>,
+      element: <div>Seller account</div>,
     },
   ]);
 
