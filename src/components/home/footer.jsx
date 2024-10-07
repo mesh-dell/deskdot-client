b36c55dbb9d8112b092551d9e42a59a953ed8df8
mesh-dell <mwaimonicas@gmail.com>
2024-10-08 00:15:04 +0300

    feat(Home): Conditionally render components depending on user role

--- src/components/home/footer.jsx
+++ src/components/home/footer.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-export default function Footer() {
+export default function Footer({ isSeller }) {
   return (
     <div className="justify-evenly space-y-7 text-center text-dark-green *:space-y-3 md:flex md:space-y-0 md:text-left">
       <div>
@@ -8,35 +8,40 @@ export default function Footer() {
         <h2>DeskDot provides top-quality office supplies.</h2>
       </div>
 
-      <div>
-        <h1 className="text-xl font-semibold">Account</h1>
-        <ul className="space-y-3">
-          <li>
-            <Link>My Account</Link>
-          </li>
-          <li>
-            <Link>Login</Link>
-          </li>
-          <li>
-            <Link>Register</Link>
-          </li>
-          <li>
-            <Link>Explore Sellers</Link>
-          </li>
-        </ul>
-      </div>
+      {!isSeller ? (
+        <div>
+          <h1 className="text-xl font-semibold">Account</h1>
+
+          <ul className="space-y-3">
+            <li>
+              <Link to={"/account"}>My Account</Link>
+            </li>
+            <li>
+              <Link to={"/signin"}>Login</Link>
+            </li>
+            <li>
+              <Link to={"/signup"}>Register</Link>
+            </li>
+            <li>
+              <Link>Explore Sellers</Link>
+            </li>
+          </ul>
+        </div>
+      ) : (
+        <></>
+      )}
 
       <div>
         <h1 className="text-xl font-semibold">Seller</h1>
         <ul className="space-y-3">
           <li>
-            <Link>My Account</Link>
+            <Link to={"/account/seller"}>My Account</Link>
           </li>
           <li>
-            <Link>Login</Link>
+            <Link to={"/signin/seller"}>Login</Link>
           </li>
           <li>
-            <Link>Register</Link>
+            <Link to={"/signup/seller"}>Register</Link>
           </li>
         </ul>
       </div>
--- src/pages/buyer/home.jsx
+++ src/pages/buyer/home.jsx
@@ -1,14 +1,18 @@
 import NavBar from "../../components/home/navbar";
+import NavBarSeller from "../../components/seller/navbar";
 import HeroesSection from "../../components/home/heroes";
 import ProductSection from "../../components/home/productSection";
 import CallToAction from "../../components/home/cta";
 import Footer from "../../components/home/footer";
 
 export default function HomePage() {
+  const isSeller = true;
   return (
     <div className="mx-6 *:mb-10 md:mx-10">
-      <NavBar /> <HeroesSection /> <ProductSection /> <CallToAction />
-      <Footer />
+      {isSeller ? <NavBarSeller /> : <NavBar />} <HeroesSection />
+      {!isSeller ? <ProductSection /> : <></>}
+      <CallToAction />
+      <Footer isSeller = {isSeller}/>
     </div>
   );
 }
