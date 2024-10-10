7e8d151142e11f3039e857adca73468e9077e106
mesh-dell <mwaimonicas@gmail.com>
2024-10-10 15:21:01 +0300

    feat(services): Create auth service

---
+++ src/services/authService.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const API_URL = "http://127.0.0.1:5000/api/auth";
+
+const login = async (credentials) => {
+  const response = await axios.post(`${API_URL}/login`, credentials);
+  return response.data;
+};
+
+const signup = async (userData) => {
+  const response = await axios.post(`${API_URL}/register`, userData);
+  return response.data;
+};
+
+const logout = async (userData) => {
+  const response = await axios.post(`${API_URL}/logout`, userData);
+  return response.data;
+};
+
+const refreshToken = async (userData) => {
+  const response = await axios.post(`${API_URL}/refresh-token`, userData);
+  return response.data;
+};
+
+export { login, logout, signup, refreshToken };
