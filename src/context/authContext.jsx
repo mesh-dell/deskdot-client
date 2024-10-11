96fb551a8203475f77caf3b4c10d131ae2cb6f67
mesh-dell <mwaimonicas@gmail.com>
2024-10-11 22:46:29 +0300

    refactor(context): Separate services from authContext

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -1,35 +1,17 @@
 import { createContext, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import {
-  login as loginService,
-  logout as logoutService,
-} from "../services/authService";
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
-  const navigate = useNavigate();
+  const { user, setUser } = useState(null);
 
-  const login = async (credentials) => {
-    const userData = await loginService(credentials);
+  const login = (userData) => {
     setUser(userData);
-    navigate("/");
   };
 
   const logout = () => {
-    logoutService(user);
     setUser(null);
-    navigate("/");
   };
 
-  const isAuthenticated = () => {
-    return user !== null;
-  };
-
-  return (
-    <AuthContext.Provider value={{ user, login, logout, isAuthenticated }}>
-      {children}
-    </AuthContext.Provider>
-  );
+  return <AuthContext.Provider>{children}</AuthContext.Provider>;
 };
--- src/pages/buyer/home.jsx
+++ src/pages/buyer/home.jsx
@@ -6,7 +6,7 @@ import CallToAction from "../../components/home/cta";
 import Footer from "../../components/home/footer";
 
 export default function HomePage() {
-  const isSeller = true;
+  const isSeller = false;
   return (
     <div className="mx-6 *:mb-10 md:mx-10">
       {isSeller ? <NavBarSeller /> : <NavBar />} <HeroesSection />
