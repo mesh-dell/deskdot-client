99308d8d6cf3cff4f918e810943ee7af56882467
mesh-dell <mwaimonicas@gmail.com>
2024-10-12 21:38:38 +0300

    feat(product): Dynamically render product card

--- src/components/home/productCard.jsx
+++ src/components/home/productCard.jsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom";
 
-export default function ProductCard({ productId }) {
+export default function ProductCard({ productId, price, product_name }) {
   return (
     <Link to={`/shop/${productId}`}>
       <div className="flex w-full flex-col items-center space-y-2 bg-light-white p-3 text-center text-dark-green md:w-fit">
         <div className="h-48 w-52 bg-light-grey">Image</div>
-        <h1 className="text-xl">Visocin Stapeler</h1>
-        <h1 className="font-semibold">Ksh. 450</h1>
+        <h1 className="text-xl">{product_name}</h1>
+        <h1 className="font-semibold">Ksh. {price}</h1>
         <button className="rounded-md bg-dark-green px-4 py-2 text-white">
           Add to cart
         </button>
--- src/components/home/productSection.jsx
+++ src/components/home/productSection.jsx
@@ -1,15 +1,24 @@
 import ProductCard from "./productCard";
+import { getProducts } from "../../services/productService";
+import { useLoaderData } from "react-router-dom";
+
+export async function loader() {
+  const products = await getProducts();
+  return { products };
+}
 
 export default function ProductSection() {
+  const { products } = useLoaderData();
+
   return (
     <div>
       <h1 className="mb-5 text-center text-xl text-dark-green">
         Here are some of our products
       </h1>
       <div className="flex flex-col justify-around space-y-5 md:flex-row md:space-y-0">
-        <ProductCard productId={1} />
-        <ProductCard productId={2} />
-        <ProductCard productId={3} />
+        {products.products.map((product) => (
+          <ProductCard key={product.product_id} productId={product.product_id} product_name={product.product_name} price={product.price}/>
+        ))}
       </div>
     </div>
   );
