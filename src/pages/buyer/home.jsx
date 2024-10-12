f10f6e473c1c538bc003da206b05fd9c2514e6f8
mesh-dell <mwaimonicas@gmail.com>
2024-10-12 21:39:22 +0300

    feat(home): Use auth context to verify user role

--- src/pages/buyer/home.jsx
+++ src/pages/buyer/home.jsx
@@ -4,15 +4,19 @@ import HeroesSection from "../../components/home/heroes";
 import ProductSection from "../../components/home/productSection";
 import CallToAction from "../../components/home/cta";
 import Footer from "../../components/home/footer";
+import { AuthContext } from "../../context/authContext";
+import { useContext } from "react";
 
 export default function HomePage() {
-  const isSeller = false;
+  const context = useContext(AuthContext);
+  const isSeller = context.user?.user.role === "seller" ? true : false;
+
   return (
     <div className="mx-6 *:mb-10 md:mx-10">
       {isSeller ? <NavBarSeller /> : <NavBar />} <HeroesSection />
       {!isSeller ? <ProductSection /> : <></>}
       <CallToAction />
-      <Footer isSeller = {isSeller}/>
+      <Footer isSeller={isSeller} />
     </div>
   );
 }
