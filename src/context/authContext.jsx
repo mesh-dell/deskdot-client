7eef18f4cf7964a440fd1c7723bf328941e58731
mesh-dell <mwaimonicas@gmail.com>
2024-10-12 15:16:40 +0300

    feat(auth): Store userData in local storage on login

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -1,16 +1,25 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+  }, []);  
+
   const setLogin = (userData) => {
     setUser(userData);
+    localStorage.setItem("user", JSON.stringify(userData));
   };
 
   const setLogout = () => {
     setUser(null);
+    localStorage.removeItem("user");
   };
 
   return (
