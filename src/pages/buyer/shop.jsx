ae23487e3a70a45ffa7a9a87ded262e21ab0c981
mesh-dell <mwaimonicas@gmail.com>
2024-10-13 20:10:45 +0300

    feat(product): Dynamically render product cards

--- src/pages/buyer/shop.jsx
+++ src/pages/buyer/shop.jsx
@@ -1,11 +1,32 @@
 import NavBar from "../../components/home/navbar";
 import BreadCrumb from "../../components/shop/breadcrumb";
-import ProductRow from "../../components/shop/productRow";
+import ProductCard from "../../components/home/productCard";
+
+import { useLoaderData } from "react-router-dom";
+import { getProducts } from "../../services/productService";
+
+export async function loader() {
+  const products = await getProducts();
+  return { products };
+}
 
 export default function Shop() {
+  const loaderData = useLoaderData();
+  const products = loaderData.products.products;
+
   return (
     <div className="mx-6 *:mb-10 md:mx-10">
-      <NavBar /> <BreadCrumb /> <ProductRow /> <ProductRow /> <ProductRow />
+      <NavBar /> <BreadCrumb />
+      <div className="flex grid-cols-3 flex-col justify-items-center space-y-5 md:grid md:space-y-0">
+        {products.map((product) => (
+          <ProductCard
+            key={product.product_id}
+            productId={product.product_id}
+            product_name={product.product_name}
+            price={product.price}
+          />
+        ))}
+      </div>
     </div>
   );
 }
