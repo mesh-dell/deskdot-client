12b01f20e47a9ae2ea523ee8367d1ad0e33f9ef4
mesh-dell <mwaimonicas@gmail.com>
2024-10-16 23:41:55 +0300

    feat(account): Pass profile as prop from loader data

--- src/pages/buyer/account.jsx
+++ src/pages/buyer/account.jsx
@@ -2,16 +2,18 @@ import SideBar from "../../components/account/sidebar";
 import NavBar from "../../components/home/navbar";
 import ProfileMenu from "../../components/account/profileMenu";
 import LoginInfo from "../../components/account/loginInfo";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLoaderData } from "react-router-dom";
 
 export default function Account() {
+  const { profile } = useLoaderData();
+
   return (
     <div className="mx-6 *:mb-10 md:mx-10">
       <NavBar />
       <div className="items-start md:flex">
         <div>
           <div className="my-10 flex items-center justify-between md:my-0 md:block">
-            <LoginInfo />
+            <LoginInfo profile={profile}/>
             <ProfileMenu />
           </div>
           <SideBar />
