cc78a4efbf99da019061d6f4c2ec12d076eafd7a
mesh-dell <mwaimonicas@gmail.com>
2024-10-16 23:39:19 +0300

    feat(account): Dyanmically render login Info by passing by props

--- src/components/account/loginInfo.jsx
+++ src/components/account/loginInfo.jsx
@@ -1,8 +1,8 @@
-export default function LoginInfo() {
+export default function LoginInfo({profile}) {
   return (
     <div className="md:mb-10 space-y-1">
-      <h1 className="text-xl font-semibold">Julia Tavarez</h1>
-      <h2 className="text-light-grey">julia24@gmail.com</h2>
+      <h1 className="text-xl font-semibold">{profile.first_name} {profile.last_name}</h1>
+      <h2 className="text-light-grey">{profile.email}</h2>
     </div>
   );
 }
