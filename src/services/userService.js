f40d3a72c49fd812516def0eae2186e4c65ac8d4
mesh-dell <mwaimonicas@gmail.com>
2024-10-16 23:43:08 +0300

    refactor(userService): Export functions

--- src/services/userService.js
+++ src/services/userService.js
@@ -2,20 +2,6 @@ import axios from "axios";
 
 const API_URL = "http://127.0.0.1:5000/api/users";
 
-const user = {
-  message: "Logged in succesfully",
-  user: {
-    id: 1,
-    first_name: "John",
-    email: "john@example.com",
-    role: "buyer",
-  },
-  accessToken:
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxLCJyb2xlIjoiYnV5ZXIifSwiaWF0IjoxNzI4NTY0MjEyLCJleHAiOjE3Mjg1Njc4MTJ9.MZaRLJlkHQec97NdY9vtN_ifTZdtNY96CzLSSATgcSg",
-  refreshToken:
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxfSwiaWF0IjoxNzI4NTY0MjEyLCJleHAiOjE3MjkxNjkwMTJ9.FZAyvocD6gqr9MJC3CBDXEe55VAn_GmCqhRkMncvG24",
-};
-
 const getProfile = async (user) => {
   const response = await axios.get(`${API_URL}/profile`, {
     headers: {
@@ -33,3 +19,5 @@ const updateProfile = async (user, data) => {
   });
   return response.data;
 };
+
+export { getProfile, updateProfile };
