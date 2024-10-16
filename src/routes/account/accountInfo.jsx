6be115adfe9c11a517a31d78cbf9ea120d6600ca
mesh-dell <mwaimonicas@gmail.com>
2024-10-16 23:40:38 +0300

    feat(loader): Implement loader function to get profile

--- src/routes/account/accountInfo.jsx
+++ src/routes/account/accountInfo.jsx
@@ -1,10 +1,18 @@
 import PersonalInfo from "../../components/account/personalInfo";
 import RecentOrders from "../../components/account/recentOrders";
-
+import { useLoaderData } from "react-router-dom";
+import { getProfile } from "../../services/userService";
+export async function loader() {
+  const user = JSON.parse(localStorage.getItem("user"));
+  const profile = await getProfile(user);
+  return { profile };
+}
 export default function AccountInfo() {
+  const { profile } = useLoaderData();
+
   return (
     <div>
-      <PersonalInfo />
+      <PersonalInfo profile={profile} />
       <RecentOrders />
     </div>
   );
