373ea4f20319295bb2e531080a50a407d12d20a1
mesh-dell <mwaimonicas@gmail.com>
2024-10-17 22:51:22 +0300

    feat(orderDetails): Add loader method to load the order and its items

--- src/routes/account/orderDetails.jsx
+++ src/routes/account/orderDetails.jsx
@@ -8,10 +8,47 @@ import {
   TableCaption,
   TableContainer,
 } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
 import OrderItem from "../../components/orders/orderItem";
 
+import { getOrder } from "../../services/orderService";
+import { useLoaderData } from "react-router-dom";
+import { getProduct } from "../../services/productService";
+
+export async function loader({ params }) {
+  const { orderId } = params;
+  const user = JSON.parse(localStorage.getItem("user"));
+  const order = await getOrder(user, orderId);
+
+  const orderItemsWithProducts = await Promise.all(
+    order.orderItems.map(async (item) => {
+      const product = await getProduct(item.product_id);
+      return { ...item, productName: product.product_name }; // Attach product name
+    }),
+  );
+
+  return { order: { ...order, orderItems: orderItemsWithProducts } };
+}
+
 export default function OrderDetails() {
+  const formatDate = (date) => {
+    const d = new Date(date);
+    return d.toLocaleString();
+  };
+
+  const calculateTotal = (order) => {
+    let totalCost = 0;
+
+    order.orderItems.forEach((item) => {
+      const itemCost = parseFloat(item.price) * parseInt(item.quantity);
+      totalCost += itemCost;
+    });
+
+    return totalCost;
+  };
+
+  const { order } = useLoaderData();
+  console.log(order);
+
   return (
     <div className="space-y-5 text-dark-green">
       <div className="mb-5 flex items-center justify-between">
@@ -33,19 +70,24 @@ export default function OrderDetails() {
           </Thead>
           <Tbody>
             <Tr>
-              <Td>34</Td>
-              <Td>24th June 2024</Td>
-              <Td>Ksh. 779</Td>
+              <Td>{order.order.order_id}</Td>
+              <Td>{formatDate(order.order.order_date)}</Td>
+              <Td>Ksh. {calculateTotal(order)}</Td>
             </Tr>
           </Tbody>
         </Table>
       </TableContainer>
 
       <div className="grid-cols-2 gap-4 space-y-4 md:grid md:space-y-0">
-        <OrderItem />
-        <OrderItem />
-        <OrderItem />
-        <OrderItem />
+        {order.orderItems.map((item) => (
+          <OrderItem
+            key={item.order_item_id}
+            quantity={item.quantity}
+            price={item.price}
+            status={item.status}
+            name={item.productName}
+          />
+        ))}
       </div>
     </div>
   );
