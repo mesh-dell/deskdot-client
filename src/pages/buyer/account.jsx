aafa93405a4b96b2921f2820ed112e8e1ab9b910
mesh-dell <mwaimonicas@gmail.com>
2024-10-17 15:24:06 +0300

    feat(refreshToken): Handle token expiry by refreshin token

--- src/pages/buyer/account.jsx
+++ src/pages/buyer/account.jsx
@@ -3,26 +3,46 @@ import NavBar from "../../components/home/navbar";
 import ProfileMenu from "../../components/account/profileMenu";
 import LoginInfo from "../../components/account/loginInfo";
 import { Outlet, useLoaderData } from "react-router-dom";
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../../context/authContext";
 
 export default function Account() {
-  const { profile } = useLoaderData();
+  const loaderData = useLoaderData();
+  const { setLogin, user } = useContext(AuthContext);
+  const { profile } = loaderData;
+
+  useEffect(() => {
+    if (
+      loaderData?.accessToken &&
+      loaderData?.accessToken !== user?.accessToken
+    ) {
+      const updatedUser = { ...user, accessToken: loaderData.accessToken };
+      setLogin(updatedUser);
+    }
+  }, [loaderData?.accessToken, user?.accessToken, setLogin]);
 
   return (
     <div className="mx-6 *:mb-10 md:mx-10">
       <NavBar />
-      <div className="items-start md:flex">
-        <div>
-          <div className="my-10 flex items-center justify-between md:my-0 md:block">
-            <LoginInfo profile={profile}/>
-            <ProfileMenu />
+      {profile ? (
+        <div className="items-start md:flex">
+          <div>
+            <div className="my-10 flex items-center justify-between md:my-0 md:block">
+              <LoginInfo profile={profile} />
+              <ProfileMenu />
+            </div>
+            <SideBar />
           </div>
-          <SideBar />
-        </div>
 
-        <div className="w-full md:ml-20">
-          <Outlet />
+          <div className="w-full md:ml-20">
+            <Outlet />
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className="text-center text-light-grey">
+          Only registered users can access dashboard.
+        </div>
+      )}
     </div>
   );
 }
--- src/routes/account/accountInfo.jsx
+++ src/routes/account/accountInfo.jsx
@@ -2,11 +2,39 @@ import PersonalInfo from "../../components/account/personalInfo";
 import RecentOrders from "../../components/account/recentOrders";
 import { useLoaderData } from "react-router-dom";
 import { getProfile } from "../../services/userService";
+import { refreshToken } from "../../services/authService";
+
 export async function loader() {
   const user = JSON.parse(localStorage.getItem("user"));
-  const profile = await getProfile(user);
-  return { profile };
+  if (!user) {
+    return { error: "You must login to access this page" };
+  }
+  try {
+    const profile = await getProfile(user);
+    return { profile };
+  } catch (error) {
+    if (error.status === 401) {
+      // Refresh token
+      const payload = {
+        refreshToken: user.refreshToken,
+        role: "buyer",
+      };
+      try {
+        const { accessToken } = await refreshToken(payload);
+        const updatedUser = { ...user, accessToken };
+        localStorage.setItem("user", JSON.stringify(updatedUser));
+        const profile = await getProfile(updatedUser);
+        return { profile, accessToken };
+      } catch (error) {
+        if (error.status === 403) {
+          return redirect("/signin");
+        }
+      }
+    }
+    return { error };
+  }
 }
+
 export default function AccountInfo() {
   const { profile } = useLoaderData();
 
