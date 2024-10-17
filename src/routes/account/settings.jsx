ad9fc0bcd37b14f16d9a7c26d5198315c310c5e8
mesh-dell <mwaimonicas@gmail.com>
2024-10-17 17:04:12 +0300

    feat(settings): Define action function and dynamically render profile data

--- src/routes/account/settings.jsx
+++ src/routes/account/settings.jsx
@@ -1,30 +1,57 @@
 import { Input } from "@chakra-ui/react";
 import { Form } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
+import { updateProfile } from "../../services/userService";
+
+export async function action({ request }) {
+  const user = JSON.parse(localStorage.getItem("user"));
+  const formData = await request.formData();
+  const payload = { ...Object.fromEntries(formData), email: user.user.email };
+  const updatedProfile = await updateProfile(user, payload);
+  return { updatedProfile };
+}
 
 export default function Settings() {
+  const { profile } = useLoaderData();
+
   return (
     <div className="space-y-10">
       <h1 className="font-semibold">Settings</h1>
 
       <div className="space-y-1">
         <h2>Profile</h2>
-        <h3 className="text-light-grey">Logged in as julia24@gmail.com</h3>
+        <h3 className="text-light-grey">Logged in as {profile.email}</h3>
       </div>
 
       <div>
-        <Form className="space-y-8">
-          <div className="md:flex md:space-x-10 md:space-y-0 space-y-8">
+        <Form className="space-y-8" method="put">
+          <div className="space-y-8 md:flex md:space-x-10 md:space-y-0">
             <div>
               <label htmlFor="first">First Name</label>
-              <Input id="first" placeholder="Jane" />
+              <Input
+                id="first"
+                placeholder="Jane"
+                defaultValue={profile.first_name}
+                name="first_name"
+              />
             </div>
             <div>
               <label htmlFor="last">Last Name</label>
-              <Input id="last" placeholder="Doe" />
+              <Input
+                id="last"
+                placeholder="Doe"
+                defaultValue={profile.last_name}
+                name="last_name"
+              />
             </div>
           </div>
 
-          <button className="px-6 py-2 bg-dark-green text-white rounded-md" type="submit">Update Profile</button>
+          <button
+            className="rounded-md bg-dark-green px-6 py-2 text-white"
+            type="submit"
+          >
+            Update Profile
+          </button>
         </Form>
       </div>
     </div>
