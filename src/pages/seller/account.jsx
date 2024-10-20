fea25c5ed9a9a711c46ff4ae7f7f6e107df70a27
mesh-dell <mwaimonicas@gmail.com>
2024-10-20 14:53:57 +0300

    feat(accountSeller): Handle update of user state after refreshing token
    Pass profile data as props to loginInfo component

--- src/pages/seller/account.jsx
+++ src/pages/seller/account.jsx
@@ -3,15 +3,33 @@ import NavBarSeller from "../../components/seller/navbar";
 import ProfileMenuSeller from "../../components/seller/profileMenuSeller";
 import LoginInfo from "../../components/account/loginInfo";
 import { Outlet } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../../context/authContext";
 
 export default function AccountSeller() {
+  const loaderData = useLoaderData();
+
+  const { setLogin, user } = useContext(AuthContext);
+  const { profile } = loaderData;
+
+  useEffect(() => {
+    if (
+      loaderData?.accessToken &&
+      loaderData?.accessToken !== user?.accessToken
+    ) {
+      const updatedUser = { ...user, accessToken: loaderData.accessToken };
+      setLogin(updatedUser);
+    }
+  }, [loaderData?.accessToken, user?.accessToken, setLogin]);
+
   return (
     <div className="mx-6 *:mb-10 md:mx-10">
       <NavBarSeller />
       <div className="items-start md:flex">
         <div>
           <div className="my-10 flex items-center justify-between md:my-0 md:block">
-            <LoginInfo />
+            <LoginInfo profile={profile} />
             <ProfileMenuSeller />
           </div>
           <SideBarSeller />
