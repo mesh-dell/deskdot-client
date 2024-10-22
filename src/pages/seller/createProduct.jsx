da4f2c8d260ac11ac86737d4ab70e2fc2e7435a5
mesh-dell <mwaimonicas@gmail.com>
2024-10-23 01:03:26 +0300

    feat(createProduct): Define form action method for form submission

--- src/pages/seller/createProduct.jsx
+++ src/pages/seller/createProduct.jsx
@@ -1,15 +1,29 @@
-import { Form } from "react-router-dom";
+import { Form, redirect } from "react-router-dom";
 import { Input, Textarea } from "@chakra-ui/react";
+import { createProduct } from "../../services/productService";
+
+export async function action({ request }) {
+  const user = JSON.parse(localStorage.getItem("user"));
+  const formData = await request.formData();
+  const payload = Object.fromEntries(formData);
+  const product = await createProduct(payload, user);
+  return { product }, redirect("/account/seller/products");
+}
 
 export default function CreateProduct() {
   return (
     <div className="mx-auto flex h-screen flex-col items-center justify-center px-8 text-dark-green md:w-2/5">
       <h1 className="mb-2 text-lg font-semibold">Deskdot</h1>
-      <h2 className="text-light-grey mb-2">Create Product</h2>
-      <Form className="space-y-3">
+      <h2 className="mb-2 text-light-grey">Create Product</h2>
+      <Form className="space-y-3" method="post">
         <div>
           <label htmlFor="productName">Product Name</label>
-          <Input placeholder="Enter Product Name" id="productName"></Input>
+          <Input
+            placeholder="Enter Product Name"
+            id="productName"
+            name="product_name"
+            required
+          ></Input>
         </div>
         <div>
           <label htmlFor="price">Price</label>
@@ -18,17 +32,29 @@ export default function CreateProduct() {
             id="price"
             type="number"
             min={5}
+            name="price"
+            required
           ></Input>
         </div>
 
         <div>
           <label htmlFor="quantity">Quantity</label>
-          <Input placeholder="Enter quantity" id="quantity"></Input>
+          <Input
+            placeholder="Enter quantity"
+            id="quantity"
+            name="quantity"
+            required
+          ></Input>
         </div>
 
         <div>
           <label htmlFor="description">Description</label>
-          <Textarea placeholder="Enter description" id="description"></Textarea>
+          <Textarea
+            placeholder="Enter description"
+            id="description"
+            name="product_description"
+            required
+          ></Textarea>
         </div>
 
         <div className="flex flex-col text-center">
