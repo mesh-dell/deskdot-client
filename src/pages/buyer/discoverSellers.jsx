1bb4916186c4e9ae96cef61fff03c86432a3ccef
mesh-dell <mwaimonicas@gmail.com>
2024-10-23 21:29:33 +0300

    feat(discoverSeller): Define loader method to fetch sellers from api
    Dynamically render seller cards
    Pass loader data as props to seller card

--- src/pages/buyer/discoverSellers.jsx
+++ src/pages/buyer/discoverSellers.jsx
@@ -1,18 +1,32 @@
+import { useLoaderData } from "react-router-dom";
 import SellerCard from "../../components/discover/sellerCard";
 import NavBar from "../../components/home/navbar";
 
+import { getStores } from "../../services/storeService";
+
+export async function loader() {
+  const sellers = await getStores();
+  return { sellers };
+}
+
 export default function DiscoverSellers() {
+  const { sellers } = useLoaderData();
+
   return (
     <div className="mx-6 *:mb-10 md:mx-10">
       <NavBar />
       <h1 className="text-xl font-semibold text-dark-green">
         Discover Sellers
       </h1>
-      <div className="md:grid grid-cols-3 gap-5 space-y-4 md:space-y-0">
-        <SellerCard />
-        <SellerCard />
-        <SellerCard />
-        <SellerCard />
+      <div className="grid-cols-3 gap-5 space-y-4 md:grid md:space-y-0">
+        {sellers.stores.map((seller) => (
+          <SellerCard
+            key={seller.seller_id}
+            id={seller.seller_id}
+            name={seller.store_name}
+            description={seller.store_description}
+          />
+        ))}
       </div>
     </div>
   );
