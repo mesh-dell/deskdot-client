20b0fba44d0b1f76dfdadc0f9123fc6b86eaaab9
mesh-dell <mwaimonicas@gmail.com>
2024-10-23 21:09:19 +0300

    feat(logout): Define onCLick event handler that logs out user

--- src/components/seller/sidebarSeller.jsx
+++ src/components/seller/sidebarSeller.jsx
@@ -1,7 +1,24 @@
 import { NavLink } from "react-router-dom";
 import { useLocation } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../context/authContext";
+import { logout } from "../../services/authService";
+import { useNavigate } from "react-router-dom";
 
 export default function SideBarSeller() {
+  const { setLogout, user } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    const payload = {
+      refreshToken: user.refreshToken,
+      role: "buyer",
+    };
+    logout(payload);
+    setLogout();
+    navigate("/");
+  };
+
   const location = useLocation().pathname;
   return (
     <div className="hidden space-y-20 text-dark-green md:block">
@@ -42,7 +59,10 @@ export default function SideBarSeller() {
         </NavLink>
       </div>
 
-      <button className="rounded-md bg-light-green px-6 py-2 font-semibold">
+      <button
+        className="rounded-md bg-light-green px-6 py-2 font-semibold"
+        onClick={handleClick}
+      >
         Log Out
       </button>
     </div>
