60f6962239a318cff84b8727c60963feb8ed9eb7
mesh-dell <mwaimonicas@gmail.com>
2024-10-23 21:31:12 +0300

    feat(sellerCard): Dynamically render data passed down as props

--- src/components/discover/sellerCard.jsx
+++ src/components/discover/sellerCard.jsx
@@ -8,10 +8,10 @@ import {
 } from "@chakra-ui/react";
 
 import { Link } from "react-router-dom";
-export default function SellerCard() {
+export default function SellerCard({ id, name, description }) {
   return (
     <div className="flex flex-col items-center justify-evenly bg-light-white px-5 py-5 text-dark-green md:h-72 md:w-96">
-      <h1 className="font-semibold">Corvette Stationery</h1>
+      <h1 className="font-semibold">{name}</h1>
       <Accordion allowToggle className="my-5">
         <AccordionItem>
           <h2>
@@ -22,15 +22,12 @@ export default function SellerCard() {
               <AccordionIcon />
             </AccordionButton>
           </h2>
-          <AccordionPanel pb={4}>
-            Take your love for speed and style to a whole new level with our
-            exclusive Corvette stationery collection!
-          </AccordionPanel>
+          <AccordionPanel pb={4}>{description}</AccordionPanel>
         </AccordionItem>
       </Accordion>
       <Link
         className="rounded-md bg-dark-green px-4 py-2 text-white"
-        to={"/shop/seller/10"}
+        to={`/shop/seller/${id}`}
       >
         View Products
       </Link>
